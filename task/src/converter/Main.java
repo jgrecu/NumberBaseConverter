dbe3fa2515d3fdfddc42a099ab09257284cd161d
Jeremy Grecu <jeremy.grecu@sap.com>
2021-07-02 16:58:14 +0100

    JBA - Number Base Convertor - Stage 1/4 - Convert Decimals

--- task/src/converter/Main.java
+++ task/src/converter/Main.java
@@ -1,8 +1,24 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number in decimal system: ");
+        int decimal = scanner.nextInt();
+        System.out.println("Enter target base: ");
+        int base = scanner.nextInt();
+        System.out.println("Conversion result: " + Integer.toString(decimal, base));
+    }
+
+    public static String convertDecimalToBase(int decimal, int base) {
+        StringBuilder sb = new StringBuilder();
+        while (decimal > 0) {
+            sb.append(decimal % base);
+            decimal /= base;
+        }
+        return sb.reverse().toString();
     }
 }
