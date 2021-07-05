b9670ef58389676d0557b2c6dc1ab92140cc1546
Jeremy Grecu <jeremy.grecu@sap.com>
2021-07-05 12:58:57 +0100

    JBA - Number Base Convertor - Stage 2/4 Convert to decimal

--- task/src/converter/Main.java
+++ task/src/converter/Main.java
@@ -6,11 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number in decimal system: ");
-        int decimal = scanner.nextInt();
-        System.out.println("Enter target base: ");
-        int base = scanner.nextInt();
-        System.out.println("Conversion result: " + convertDecimalToBase(decimal, base));
+
+        while (true) {
+            printMenu();
+            String choice = scanner.nextLine();
+            if ("/from".equals(choice)) {
+                System.out.println("Enter number in decimal system: ");
+                int decimal = scanner.nextInt();
+                System.out.println("Enter target base: ");
+                int base = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Conversion result: " + convertDecimalToBase(decimal, base));
+                System.out.println();
+            } else if ("/to".equals(choice)) {
+                System.out.println("Enter source number: ");
+                String source = scanner.nextLine();
+                System.out.println("Enter source base: ");
+                int base = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Conversion to decimal result: " + convertBaseToDecimal(source, base));
+                System.out.println();
+            } else if ("/exit".equals(choice)) {
+                break;
+            } else {
+                continue;
+            }
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)");
     }
 
     public static String convertDecimalToBase(int decimal, int base) {
@@ -25,5 +50,9 @@ public class Main {
             decimal /= base;
         }
         return sb.reverse().toString();
+    }
+
+    public static int convertBaseToDecimal(String source, int base) {
+        return Integer.parseInt(source, base);
     }
 }
