32e6b958c7ba7e7f77f0510577886da9e618ee04
Jeremy Grecu <jeremy.grecu@sap.com>
2021-07-06 15:36:19 +0100

    JBA - Number Base Convertor - Stage 3/4 Convert to any base

--- task/src/converter/Main.java
+++ task/src/converter/Main.java
@@ -1,32 +1,23 @@
 package converter;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         while (true) {
             printMenu();
             String choice = scanner.nextLine();
-            if ("/from".equals(choice)) {
-                System.out.println("Enter number in decimal system: ");
-                int decimal = scanner.nextInt();
-                System.out.println("Enter target base: ");
-                int base = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println("Conversion result: " + convertDecimalToBase(decimal, base));
-                System.out.println();
-            } else if ("/to".equals(choice)) {
-                System.out.println("Enter source number: ");
-                String source = scanner.nextLine();
-                System.out.println("Enter source base: ");
-                int base = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println("Conversion to decimal result: " + convertBaseToDecimal(source, base));
-                System.out.println();
-            } else if ("/exit".equals(choice)) {
+            String[] choices = choice.split(" ");
+            if (choices.length > 1) {
+                int srcBase = Integer.parseInt(choices[0]);
+                int dstBase = Integer.parseInt(choices[1]);
+                runConversion(srcBase, dstBase);
+            } else if (choices.length == 1 && choices[0].equals("/exit")) {
+                scanner.close();
                 break;
             } else {
                 continue;
@@ -35,33 +26,48 @@ public class Main {
     }
 
     public static void printMenu() {
-        System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)");
+        System.out.println("Enter two numbers in format: {source base} {target base} (To quit type /exit) ");
     }
 
-    public static String convertDecimalToBase(int decimal, int base) {
-        StringBuilder sb = new StringBuilder();
-        while (decimal > 0) {
-            int reminder = decimal % base;
-            if (base == 16 && reminder > 9) {
-                sb.append((char) ('a' + reminder - 10));
-            } else {
-                sb.append(reminder);
+    public static void runConversion(int src, int dst) {
+        while (true) {
+            System.out.println("Enter number in base " + src + " to convert to base " + dst + " (To go back type /back) ");
+            String choice = scanner.nextLine();
+            if ("/back".equals(choice)) {
+                break;
             }
-            decimal /= base;
+            if (src == 10) {
+                BigInteger decimal = new BigInteger(choice);
+                System.out.println("Conversion result: " + convertDecimalToBase(decimal, dst));
+                System.out.println();
+            } else if (dst == 10) {
+                System.out.println("Conversion result: " + convertBaseToDecimal(choice, src));
+            } else {
+                System.out.println("Conversion result: " + convertDecimalToBase(convertBaseToDecimal(choice, src), dst));
+            }
+        }
+    }
+
+    public static String convertDecimalToBase(BigInteger decimal, int base) {
+        StringBuilder sb = new StringBuilder();
+        String digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        while (!BigInteger.ZERO.equals(decimal)) {
+            int reminder = Integer.parseInt(decimal.mod(BigInteger.valueOf(base)).toString());
+            char c = digits.charAt(reminder);
+            sb.append((c + "").toLowerCase());
+            decimal = decimal.divide(BigInteger.valueOf(base));
         }
         return sb.reverse().toString();
     }
 
-    public static int convertBaseToDecimal(String source, int base) {
-        //return Integer.parseInt(source, base);
+    public static BigInteger convertBaseToDecimal(String source, int base) {
         String reversedSource = new StringBuilder(source).reverse().toString().toUpperCase();
-        int decimal = 0;
-        String digits = "0123456789ABCDEF";
+        BigInteger decimal = BigInteger.ZERO;
+        String digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         for (int i = 0; i < reversedSource.length(); i++) {
             char c = reversedSource.charAt(i);
             int d = digits.indexOf(c);
-
-            decimal += (int) Math.pow(base, i) * d;
+            decimal = decimal.add(BigInteger.valueOf((long) Math.pow(base, i) * d));
         }
         return decimal;
     }
